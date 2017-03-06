ff4c4182d76d4f18b34b8bbe2d99b78fb32824c1
Tolmatc <mihail910@mail.ru>
2017-03-05 20:40:53 -0600

    fghjktyuio

--- hw8/src/index.js
+++ hw8/src/index.js
@@ -1,33 +1,11 @@
 require('./index.css');
-/*
-function callAPI(method, params) {
-    return new Promise((resolve, reject) => {
-        VK.api(method, params, function(result) {
-            if (result.error) {
-                reject();
-            } else {
-                resolve(result.response);
-            }
-        });
-    });
-}
-*/
-// var friendsList = document.querySelector('#friends');
-// var loadFriends = document.querySelector('#loadFriends');
-
-// loadFriends.addEventListener('click', () => {
-//     login()
-//         .then(() => callAPI('friends.get', { v: 5.62, fields: ['city', 'country', 'photo_100'] }))
-//         .then(result => friendsList.innerHTML = createFriendsDiv(result.items))
-//         .then(() => loadFriends.parentNode.removeChild(loadFriends))
-//         .catch(() => alert('всё плохо'));
-// });
-
 // 5901122
 // Все на промисах (каждый запрос), т.к. на каждой стадии запроса что-то может пойти не так
 
 var loadFriends = document.querySelector('#loadFriends');
 var leftList = document.querySelector('.friends_item_left_inner');
+var rightList = document.querySelector('.friends_item_right_inner');
+
 // Инициализация приложения и проверка - разрешили нам или нет
 function login() {
     return new Promise(function (resolve, reject) {
@@ -35,7 +13,7 @@ function login() {
             apiId: 5901122
         })
         VK.Auth.login(function(result) {
-            if(result.status == 'connected') {
+            if (result.status == 'connected') {
                 resolve();
             } else {
                 reject(result.error);
@@ -48,7 +26,7 @@ function login() {
 function getFriends(method, params) {
     return new Promise(function(resolve, reject) {
         VK.Api.call(method, params, function(result) {
-            if(result.error) {
+            if (result.error) {
                 reject(result.error);
             } else {
                 resolve(result.response);
@@ -56,6 +34,7 @@ function getFriends(method, params) {
         })
     })
 }
+
 // Тут создаем DIV с другом. Берем шаблон через require
 // Возвращаем шаблон. В handlebars передается объект, который можно крутить через синтаксис
 // hbs = {{#each ПАРАМЕТР-объект}}
@@ -67,132 +46,139 @@ function createFriendsDiv(friends) {
     });
 }
 
+function setElementDraggable() {
+    var draggableElements = document.querySelectorAll('.one_friend');
+
+    for (var i = 0; i < draggableElements.length; i++) {
+        draggableElements[i].setAttribute('draggable', 'true');
+        draggableElements[i].setAttribute('ondragstart', 'drag(event)');
+        draggableElements[i].setAttribute('id', i+1)
+        draggableElements[i].setAttribute('ondrop', 'false')
+    }
+}
+
+window.drag = function(ev) {
+    ev.dataTransfer.setData('text', ev.target.id);
+}
+
+window.drop = function(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData('text');
+
+    ev.target.appendChild(document.getElementById(data));
+}
+
+window.deleteFriend = function(ev) {
+    ev.preventDefault();
+
+    var data = ev.dataTransfer.getData('text');
+
+    return new Promise(function(resolve, reject) {
+        VK.Api.call('friends.delete', {user_id: 394404856} , function(result) {
+            console.log(result)
+        })
+    })
+}
+
+window.allowDrop = function(ev) {
+    ev.preventDefault();
+}
+
 loadFriends.addEventListener('click', function() {
     login()
         .then(
-            () => getFriends('friends.get', { v: 5.62, fields: ['city', 'country', 'photo_100'] })
+            () => getFriends('friends.get', { v: 5.62,fields: ['city', 'country', 'photo_100', 'user_id', 'order: random'] })
         )
         .then(
             (result) => leftList.innerHTML = createFriendsDiv(result.items)
         )
-        .catch(
-            () => alert('Что-то пошло не так - ' + result.error)
+        .then(
+            () => setElementDraggable()
         )
 })
 
-var left_friends_container = document.querySelector('.friends_item_left');
-var left_friends = left_friends_container.querySelectorAll('.one_friend');
+var leftSearchInput = document.querySelector('.friends_item_search_left_input');
+var rightSearchInput = document.querySelector('.friends_item_search_right_input');
 
-
-var right_friends_container = document.querySelector('.friends_item_right');
-
-
-
-var bigLayout = document.querySelector('.layout ');
-// var test = document.querySelector('#test');
-
-// function me(){
-//     var left_friends_container = document.querySelector('.friends_item_left');
-//     var left_friends = left_friends_container.querySelectorAll('.one_friend');
-
-//     console.log(left_friends_container);
-//     console.log(left_friends);
-// }
-
-// test.addEventListener('click', me);
-
-var movable = true;
-
-var thisFriendCard;
-
-function getStartCoordinates(elem) {
-    var thisFriendCardStartX = elem.getBoundingClientRect().left;
-    var thisFriendCardStartY = elem.getBoundingClientRect().top;
-
-    var coordinates = {
-        x: thisFriendCardStartX,
-        y: thisFriendCardStartY
-    }
-
-    return coordinates;
-}
-// Нажмем на список, выберем среди друзей одного из друзей. 
-leftList.addEventListener('mousedown', function(e) {
-
-    movable = true;
-
-    thisFriendCard = findParent(e.target);
-
-// Функция вернем объект с начальными координатами
-    // getStartCoordinates(thisFriendCard)
-
-    // console.log(getStartCoordinates(thisFriendCard))
-
-    thisFriendCard.style.position = 'absolute';
-
-
-
-    // И будем таскать его по странице
-    document.addEventListener('mousemove', function(e) {
-        if(movable) {
-            makeMovingBlock(e, thisFriendCard);
-        }
-    })
-
-});
-
-bigLayout.addEventListener('mouseup', function(e) {
-
-    movable = false;
-
-
-        // Где правый блок?? Куда будем помещать карточки друзей?
-
-    var right_friends_container_coord = {
-        fromTop : right_friends_container.offsetHeight,
-        fromLeft : right_friends_container.offsetWidth
-    }
-
-    // thisFriendCard.style.position = 'static';
-
-    var movedContainerX = thisFriendCard.offsetWidth;
-    var movecontainerY = thisFriendCard.offsetHeight;
-
-    var rigthColumnTop = right_friends_container_coord.fromTop;
-    var rightColumnLeft = right_friends_container_coord.fromLeft;
-
-    console.log('Координаты правого контейнера сверху ' + rigthColumnTop);
-    console.log('Координаты таскаемого контейнера сверху ' + movedContainerX);
-
-    console.log('__________')
-
-    console.log('Координаты правого контейнера слева ' + rightColumnLeft);
-    console.log('Координаты таскаемого контейнера слева ' + movedContainerX); 
-
-    console.log('№№№№№№№№№№№№№№№№№№№№№');
-
-})
-
-
-
-// Задаем координаты блока для перемещения относительно страничи
-function makeMovingBlock(e, target) {
-
-    target.style.left = e.pageX - target.offsetWidth / 2 + 'px';
-    target.style.top = e.pageY - target.offsetHeight / 2 +'px';
-}
-
-
-// Ищем блок с классом one_friend.
-function findParent(elem) {
-    var elemParent;
-
-    if (elem.classList.contains('one_friend')) {
+// Ищем блок - родитель с классом one_friend.
+function findParent(elem, classToFind) {
+    if (elem.classList.contains(classToFind)) {
 
         return elem;
-    } else {
-        elem = elem.parentNode;
+    }
+    elem = elem.parentNode;
 
-        return findParent(elem);
+    return findParent(elem, classToFind);
+
+}
+// Клик по плюсику - из левой колонки переносим карточку элементу в правую колонку
+leftList.addEventListener('click', leftPlusClick);
+
+function leftPlusClick(e) {
+    // Если target содержит класс нашей картинки или класс блока, в котором эта картинка
+    if (e.target.classList.contains('one_friend_item_plus') || e.target.classList.contains('one_friend_item_plus_image')) {
+        rightList.appendChild(findParent(e.target, 'one_friend')) // .one_friend
+    } else {
+        return;
     }
 }
+// Клик по крестику в правой колонке - будем удалять из правой, перемещать в левую
+rightList.addEventListener('click', rightCossClick);
+
+function rightCossClick(e) {
+    if (e.target.classList.contains('one_friend_item_plus') || e.target.classList.contains('one_friend_item_plus_image')) {
+        // Возврат в список будет в начало этого списка, а не в конец как через appendChild
+        // Пока я не понл как возвращать элементы именно на свое место.. 
+        // можно как-то по id, т.к. каждому я присвоил свой номер в виде id..хм..
+        leftList.insertBefore(findParent(e.target, 'one_friend'), leftList.children[0])    
+    } else {
+        return;
+    }
+}
+
+// Функция поиска совпадений по части слова
+function isMatching(what, where) {
+
+    var whatEqual = what.toLowerCase();
+    var whereEqual = where.toLowerCase();
+
+    var matchValue = whatEqual.indexOf(whereEqual);
+
+    if (matchValue > -1) {
+        return true;
+    }
+
+    return false;
+}
+// Функция поиска по имени или фамилии в левой колонке
+leftSearchInput.addEventListener('keyup', function(e) {
+    e.preventDefault();
+    var counter = 0;
+
+    var friendsItem = document.querySelectorAll('.friends_item_left_inner .one_friend');
+
+    for (var i = 0; i < friendsItem.length; i++) {
+        if (!isMatching(friendsItem[i].innerText, leftSearchInput.value)) {
+            friendsItem[i].style.display = 'none';
+        } else {
+            friendsItem[i].style.display = 'block';
+            counter = counter + 1;
+        }
+    }
+})
+// Функция поиска по имени или фамилии в правой колонке
+rightSearchInput.addEventListener('keyup', function(e) {
+    e.preventDefault();
+
+    var counter = 0;
+    var friendsItem = document.querySelectorAll('.friends_item_right_inner .one_friend');
+
+    for (var i = 0; i < friendsItem.length; i++) {
+        if (!isMatching(friendsItem[i].innerText, rightSearchInput.value)) {
+            friendsItem[i].style.display = 'none';
+        } else {
+            friendsItem[i].style.display = 'block';
+            counter = counter + 1;
+        }
+    }
+})
