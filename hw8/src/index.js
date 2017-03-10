f3ad58423b3bfbb70ec32fdbd4f9699607d9821e
m.tolmats <mihail910@mail.ru>
2017-03-10 11:36:47 +0300

    s

--- hw8/src/index.js
+++ hw8/src/index.js
@@ -9,9 +9,11 @@ var rightList = document.querySelector('.friends_item_right_inner');
 // Инициализация приложения и проверка - разрешили нам или нет
 function login() {
     return new Promise(function (resolve, reject) {
+        // Инициализщируем API
         VK.init({
             apiId: 5901122
         })
+        // Логинимся ВК. Если ОК, то resolve();
         VK.Auth.login(function(result) {
             if (result.status == 'connected') {
                 resolve();
@@ -46,6 +48,9 @@ function createFriendsDiv(friends) {
     });
 }
 
+// Эта функция запустится, когда шаблон с друзьями уже отработал, есть друзья с классами. 
+// Делаем каждый элемент "двигательным"(или двигаемым, как сказать-то) путем задания ему
+// draggable='true"
 function setElementDraggable() {
     var draggableElements = document.querySelectorAll('.one_friend');
 
@@ -57,6 +62,8 @@ function setElementDraggable() {
     }
 }
 
+// На "драггабельном" элементе вызываем функцию, которая 
+// будет получать данные об этом элементе. Такой уж стандарт HTML5 drag-n-drop
 window.drag = function(ev) {
     ev.dataTransfer.setData('text', ev.target.id);
 }
@@ -66,19 +73,26 @@ window.drop = function(ev) {
     var data = ev.dataTransfer.getData('text');
 
     ev.target.appendChild(document.getElementById(data));
+
+    var childrensRightInner = document.getElementById('friends_item_right_inner_block').children;
+    for(var i = 0; i < childrensRightInner.length; i++) {
+        childrensRightInner[i].removeAttribute('draggable');
+    }
 }
 
-window.deleteFriend = function(ev) {
-    ev.preventDefault();
+// Тут мы на корзину повесим такой обработчик, который позволит перетаскивать друга в корзину.
+// и повесим функцию на корзину как раз.
+// window.deleteFriend = function(ev) {
+//     ev.preventDefault();
 
-    var data = ev.dataTransfer.getData('text');
+//     var data = ev.dataTransfer.getData('text');
 
-    return new Promise(function(resolve, reject) {
-        VK.Api.call('friends.delete', {user_id: 394404856} , function(result) {
-            console.log(result)
-        })
-    })
-}
+//     return new Promise(function(resolve, reject) {
+//         VK.Api.call('friends.delete', {user_id: 44874374} , function(result) {
+//             console.log(result)
+//         })
+//     })
+// }
 
 window.allowDrop = function(ev) {
     ev.preventDefault();
@@ -86,6 +100,8 @@ window.allowDrop = function(ev) {
 
 
 loadFriends.addEventListener('click', function() {
+    // Открывает окно с друзьями
+    document.querySelector('.layout').classList.remove('hidden');
     login()
         .then(
             () => getFriends('friends.get', { v: 5.62,fields: ['city', 'country', 'photo_100', 'user_id', 'order: random'] })
@@ -96,6 +112,13 @@ loadFriends.addEventListener('click', function() {
         .then(
             () => setElementDraggable()
         )
+})
+
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('layout_container_close') || e.target.getAttribute('src') == 'img/cross.png') {
+        // Закрывает окно с друзьями
+        document.querySelector('.layout').classList.remove('hidden');
+    }
 })
 
 var leftSearchInput = document.querySelector('.friends_item_search_left_input');
@@ -152,14 +175,16 @@ function isMatching(what, where) {
 
     return false;
 }
+
 // Функция поиска по имени или фамилии в левой колонке
 leftSearchInput.addEventListener('keyup', function(e) {
     e.preventDefault();
     var counter = 0;
-
+    // Выбираем друзей в левой колонке
     var friendsItem = document.querySelectorAll('.friends_item_left_inner .one_friend');
 
     for (var i = 0; i < friendsItem.length; i++) {
+        // Нет совпадений между набранным и именем? значит скрываем этот элемент
         if (!isMatching(friendsItem[i].innerText, leftSearchInput.value)) {
             friendsItem[i].style.display = 'none';
         } else {
@@ -185,3 +210,27 @@ rightSearchInput.addEventListener('keyup', function(e) {
     }
 })
 
+var layout_container_bottom_save = document.querySelector('.layout_container_bottom_save');
+var localStorageDataNumber = 0;
+
+layout_container_bottom_save.addEventListener('click', function() {
+
+    // var friends_left = document.querySelectorAll('.friends_item_left_inner .one_friend');
+    // console.log(friends_left)
+
+    // var friends_right = document.querySelectorAll('.friends_item_right_inner .one_friend')
+
+    // console.log(friends_right)
+    history.pushState({
+        prop1: 'value1'
+        }, 'stateName', 'someurl');
+
+    console.log('saved??')
+    console.log('??')
+
+})
+
+
+window.onpopstate = function(e) {
+    console.log(e.state);
+}
