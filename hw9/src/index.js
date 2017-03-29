71ce4ada62f6bdc5778b359525f640ccb64a1a85
m.tolmats <mihail910@mail.ru>
2017-03-29 16:16:35 +0300

    s

--- hw9/src/index.js
+++ hw9/src/index.js
@@ -1,8 +1,9 @@
 require('./index.css');
 
 ymaps.ready(init);
-var myMap,
-    myPlacemark;
+var myMap;
+var myObjects = [];
+var counter = 0;
 
 function init(){     
     myMap = new ymaps.Map("map", {
@@ -10,100 +11,85 @@ function init(){
         zoom: 12
     });
 
-    // Создаем собственный макет с информацией о выбранном геообъекте.
-    var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
-        // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
-        '<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
-            '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
-            '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
-    );
-
-
-
-
-    // Создал кластер. Срисовал с примера яндекса
-    var clusterer = new ymaps.Clusterer({
-        clusterDisableClickZoom: true,
-        clusterOpenBalloonOnClick: true,
-        // Устанавливаем стандартный макет балуна кластера "Карусель".
-        clusterBalloonContentLayout: 'cluster#balloonCarousel',
-        // Устанавливаем собственный макет.
-        clusterBalloonItemContentLayout: customItemContentLayout,
-        // Устанавливаем режим открытия балуна. 
-        // В данном примере балун никогда не будет открываться в режиме панели.
-        clusterBalloonPanelMaxMapArea: 0,
-        // Устанавливаем размеры макета контента балуна (в пикселях).
-        clusterBalloonContentLayoutWidth: 200,
-
-        // Устанавливаем максимальное количество элементов в нижней панели на одной странице
-        clusterBalloonPagerSize: 5
-    });
-
-    // var MyBalloonContentLayoutClass = ymaps.templateLayoutFactory.createClass(
-    //     '<h3>Теоретически, это должен быть лэйаут балуна</p>' 
-    // );
-
-    // var balloon = new ymaps.Balloon(myMap, {
-    //     autoPan : true,
-    //     closeButton: true,
-    //     balloonContentLayout: MyBalloonContentLayoutClass
-    // });
-
-    // balloon.options.setParent(myMap.options);
-    // balloon.setData('dataaaaaaa');
-
-    // Делаю метку. точнее стилизую
-    myMap.events.add('click', function(e) {     
-        var coords = e.get('coords');
-        var geoObject = e.get('target');
-        // Открыть балун по клику на карте
-        // balloon.open(coords);
-        // Автосмещение балуна
-        // balloon.autoPan();
-
-        // balloon.getData();
-
     
 
- // Создаем новую метку  
-        myPlacemark = new ymaps.Placemark(coords, {
-            balloonContent: '<div class="balonblock_leavefeed"><p class="balonblock_leavefeed_title">Ваш отзыв</p><input type="text" placeholder="Ваше имя" class="yourname baloonInput"><input type="text" placeholder="Укажите место" class="yourplace baloonInput"><textarea placeholder="Поделитесь вашими впечатлениями" class="yourminds baloonInput"></textarea></div>',
-            balloonContentFooter: '<span class="balonblock_leavefeed_add_btn">Добавить</span>',
-            }, {
-                // Опции.
-                // Необходимо указать данный тип макета.
-                iconLayout: 'default#image',
-                // Своё изображение иконки метки.
-                iconImageHref: 'img/1.png',
-                // Размеры метки.
-                iconImageSize: [30, 42],
-                // Смещение левого верхнего угла иконки относительно
-                // её "ножки" (точки привязки).
-                iconImageOffset: [-5, -38]
-            }
-        );
-// Поведение метки при наведении курсора на нее
-        myPlacemark.events
-            .add('mouseenter', function(e) {
-                e.get('target').options.set('iconImageHref', 'img/1_orange.png')
-            })
-            .add('mouseleave', function(e) {
-                e.get('target').options.set('iconImageHref', 'img/1.png')
-            })
-// Задаем метке адрес. В Хедер пишем адрес
-        ymaps.geocode(coords).then(function(res) {
-            var a = res.geoObjects.get(0).properties.get('name');
-            myPlacemark.properties.set({
-                balloonContentHeader: a
-            })
+    myMap.events.add('click', function(e) {   
+
+
+        var customBalloonFooter = ymaps.templateLayoutFactory.createClass(`Футер ________`)
+
+        var myObjectManager = new ymaps.ObjectManager({
+            clusterize: true,
+            // Опишем метку
+            geoObjectIconLayout: 'default#image',
+            geoObjectIconImageHref: 'img/1.png',
+            geoObjectDraggable: true,
+            geoObjectIconImageSize: [36, 50],
+            geoObjectBalloonContentFooter: customBalloonFooter, // ПОЧЕМУ ШАБЛОН НЕ ПРИМЕНЯЕТСЯ? НЕ ВСТАВЛЯЕТСЯ СЮДА!
+            
+
+            // По клику на кластер - не увеличиваем карту
+            clusterDisableClickZoom: true,
+            // Тип кластера - карусель
+            clusterBalloonContentLayout: "cluster#balloonCarousel",
+            // Количество Item'ов в кластере -карусели
+            clusterBalloonPagerSize: 6,
+        });
+
+
+        myMap.geoObjects.add(myObjectManager);
+
+
+        var coordinates = e.get('coords');
+
+        // var a = ymaps.geocode([55.75, 37.62]).then(function(res) {
+        //     var a = res.geoObjects.get(0).properties.get('name');
+        //     return a;
+        // })      
+    
+        var feature = {
+            "type": "Feature",
+            'id': counter++,
+            "geometry": {
+                "type": "Point",
+                "coordinates": coordinates
+            },
+            'properties': {
+                "hintContent": "Текст подсказки",
+                "balloonContentHeader": 'ХЕДЕР',
+                "balloonContentBody": 'Боди',
+                // balloonContentFooter: customBalloonFooter // СЮДА ПОПАДАЕТ ФУНКЦИЯ В ГОЛОМ ВИДЕ, ТОЧНЕЕ ЕЕ КОД, ПОЧЕМУ?
+            },
+            // Это будут отзывы в этой feature
+            // 'feedbacks': {
+            //     name: 'Vasya'
+            // }
+        }
+
+
+        myObjectManager.objects.events.add('click', function(e) {
+            var placemarkID = e.get('objectId');
+            var overlay = myObjectManager.objects.overlays.getById(placemarkID);
+            // Это лежит в контенте боди
+            var toChtoLEzhitVBODI = overlay.getData().properties.balloonContentBody;
+            // Свойство отзывов. которых поначалу нет
+            var allThisPlaceFeedbacks = overlay.getData().feedbacks;
+            
         })
 
-// Добавляем метку в кластер
-        clusterer.add(myPlacemark); 
+        myObjects.push(feature);
 
-// Добавляем кластер в геообъект
-        myMap.geoObjects.add(clusterer);
+        var collection = {
+            "type": "FeatureCollection",
+            "features": myObjects
+        };
 
-    })    
+        myObjectManager.removeAll().add(collection);
+
+    })
 }
+
+
+
+
 
