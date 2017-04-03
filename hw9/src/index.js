62dc4d57ee935bb03e04aea7c71780faabaffe56
m.tolmats <mihail910@mail.ru>
2017-04-03 18:56:37 +0300

    hwOM

--- hw9/src/index.js
+++ hw9/src/index.js
@@ -4,6 +4,9 @@ ymaps.ready(init);
 var myMap;
 var myObjects = [];
 var counter = 0;
+var collection; 
+var myObjectManager;
+var feature;
 
 function init() {     
     myMap = new ymaps.Map('map', {
@@ -12,20 +15,65 @@ function init() {
     });
 
     var customCluster = ymaps.templateLayoutFactory.createClass(`
-        {{properties.myAdressHeader}}
-    `);
+        <div class="clusterBalloon">
+            {{properties.thisFeedback[0].place}}
+            <div class="thisadressgotoballoon">
+                {{properties.myAdressHeader}}
+            </div>
+            {{properties.thisFeedback[0].text}}
+            <div class="clusterDate">
+                {{properties.thisFeedback[0].date}}
+            </div>
+        </div>
+    `,{
+        build: function() {
+            customCluster.superclass.build.call(this);
+            $('.thisadressgotoballoon').bind('click', this.onAdressClick);
+        },
+        clear: function() {
+            $('.thisadressgotoballoon').unbind('click', this.onAdressClick);
+            customCluster.superclass.clear.call(this);
+        },
 
-    var myObjectManager = new ymaps.ObjectManager({
+        onAdressClick: function(e) {
+            
+            var clickedAdress = e.target.innerHTML.trim();
+
+            var thisClusterCollection = myObjectManager.clusters.balloon.getData();
+
+            var adresses = [];
+            var ids = [];
+            var idToOpenBalloon;
+
+            for(var i = 0; i < thisClusterCollection.features.length; i++) {
+                adresses.push(thisClusterCollection.features[i].properties.myAdressHeader)
+                ids.push(thisClusterCollection.features[i].id);
+            }
+
+            for( var k = 0; k < adresses.length; k++) {
+                if(adresses[k] == clickedAdress) {
+                    idToOpenBalloon = ids[k];
+                    break;
+                };
+            }
+
+            myObjectManager.objects.balloon.open(idToOpenBalloon);
+     
+        }
+    });
+
+    myObjectManager = new ymaps.ObjectManager({
         clusterize: true,  // кластеризация ВКЛ
         geoObjectIconLayout: 'default#image', // хрен знает что это
         iconImageHref: 'img/1.png', // Картинка для метки отработала, показывается как надо
-        geoObjectIconImageSize: [36, 50], // Размер картинки тоже отработал как ни странно
+        iconImageOffset: [0, 0],
+        geoObjectIconImageSize: [28, 40], // Размер картинки тоже отработал как ни странно
         clusterDisableClickZoom: true, // По клику на кластер НЕТ зума
         clusterBalloonContentLayout: 'cluster#balloonCarousel', // кластер у нас типа карусель
-        clusterBalloonPagerSize: 6,     // Количество элементов в кластере карусели (на одну страницу)
+        clusterBalloonPagerSize: 5,     // Количество элементов в кластере карусели (на одну страницу)
         clusterBalloonItemContentLayout: customCluster
     });
-
+   
 // Для того чтобы отобразить объекты на карте, необходимо добавить менеджер в коллекцию объектов карты.
     myMap.geoObjects.add(myObjectManager);
     myMap.events.add('click', function(e) {
@@ -80,11 +128,13 @@ function init() {
                     $('.balonblock_leavefeed_add_btn').bind('click', this.onButtonAddClick);
                     $('.ymaps-2-1-48-balloon__close-button').bind('click', this.onCloseButtonClick);
                 },
+
                 clear: function () {
                     $('.balonblock_leavefeed_add_btn').unbind('click', this.onButtonAddClick);
                     $('.ymaps-2-1-48-balloon__close-button').unbind('click', this.onCloseButtonClick);
                     BalloonContentLayout.superclass.clear.call(this);
                 },
+
                 onButtonAddClick: function () {
                     var name = $('.yourname').val();
                     var place = $('.yourplace').val();
@@ -95,43 +145,79 @@ function init() {
                     var day = now.getDate();
                     var hour = now.getHours();
                     var minutes = now.getMinutes();
-                    var date = year +'.'+month+'.'+day+' '+hour+':'+minutes
-                    // Это вот доступ к feedbacks
-                    // var thisFeatureFeedbacks = myObjectManager.objects.balloon.getData().properties.feedbacks;
-                    // Существующие отзывы
-                    // var existedFeedbacks = this.parentNode.parentNode.previousElementSibling;
-// FEATURE
-                    var feature = myObjectManager.objects.balloon.getData();       
+                    var date = year +'.'+month+'.'+day+' '+hour+':'+minutes;
+                    // FEATURE
+                    var feature = myObjectManager.objects.balloon.getData();
+                    var firstFeatureLength = feature.properties.feedbacks.length;
+        
+                    if (firstFeatureLength < 1 ) {
+                        if (name && place && text) {
+                            feature.properties.feedbacks.push({
+                                name: name,
+                                place: place,
+                                date: date,
+                                text: text
+                            });
 
-                    console.log(myObjectManager.objects.balloon);
+                            feature.properties.thisFeedback.push({
+                                name: name,
+                                place: place,
+                                date: date,
+                                text: text
+                            })
+                        }
 
-                    if (name && place && text) {                        
+                        myObjectManager.objects.balloon.open(0);
+
+                    } else {
                         feature.properties.feedbacks.push({
                             name: name,
                             place: place,
-                            text: text,
-                            date: date
+                            date: date,
+                            text: text
                         });
-                    } else {
-                        alert('Необходимо ввести значения во все поля');
-                    }
 
-
-                    
-    // При изменении фичи тут же ее откроем
-                    var overlay = myObjectManager.objects.getById(feature.id);
-
-                    myObjectManager.objects.balloon.open(overlay.id);
-
-
-                },
-                onCloseButtonClick: function() {
-                    for (var i = 0; i < myObjects.length; i++) {
-                        if (myObjects[i].properties.feedbacks.length < 1) {
-                            myObjects.splice(i, 1);
+                        var feature2 = {
+                            type: 'Feature',
+                            id: counter++,                        
+                            geometry: {
+                                type: 'Point',
+                                coordinates: feature.geometry.coordinates
+                            },
+                            // Здесь просто какие-то данные, к которым можно обратиться
+                            properties: {
+                            // Получили адрес
+                                myAdressHeader: feature.properties.myAdressHeader,
+                                thisFeedback: [{
+                                    name: name,
+                                    place: place,
+                                    date: date,
+                                    text: text
+                                }]
+                            }
                         }
+
+                        myObjects.push(feature2);
+
+                        collection = {
+                            'type': 'FeatureCollection',
+                            'features': myObjects
+                        }
+
+                        myObjectManager.removeAll().add(collection);
                     }
-    // Если кликаем по кнопке "закрыть", то обновляем нашу коллекцию и отрисовываем ранее добавленные штуки
+
+                    // При изменении фичи тут же ее откроем
+                    var overlay = myObjectManager.objects.getById(feature.id);
+                    myObjectManager.objects.balloon.open(overlay.id);
+                },
+
+                onCloseButtonClick: function() {
+// Если у конкретного открытого балуна ничего нет в отзывах, значит он пустой. И мы его удалим. Т.к. он открыт, то он и есть последний. Его и удаляем путем уменьшения длины массива объектов в коллекции
+                    if(myObjectManager.objects.balloon.getData().properties.feedbacks[0] == undefined) {
+                        myObjects.length = myObjects.length - 1;
+                    }
+
                     var collection = {
                         'type': 'FeatureCollection',
                         'features': myObjects
@@ -144,13 +230,15 @@ function init() {
 // Устанавливаем шаблон для балуна
         myObjectManager.objects.options.set({
             // В customBallon надо отрисовать все 
-            balloonContentLayout: BalloonContentLayout
+            balloonContentLayout: BalloonContentLayout,
+            balloonMinHeight: 530
         })
 
 // В шапку пишем Название Адреса
         ymaps.geocode(coordinates).then(function(res) {
             var thisAdress = res.geoObjects.get(0).properties.get('name');
-            var feature = {
+
+            feature = {
                 type: 'Feature',
                 id: counter++,                        
                 geometry: {
@@ -161,13 +249,14 @@ function init() {
                 properties: {
                 // Получили адрес
                     myAdressHeader: thisAdress,
-                    feedbacks: []
+                    feedbacks: [],
+                    thisFeedback: []
                 }
             }
-
+            // var feature = createNewFeature(coordinates, thisAdress);
             myObjects.push(feature);
 
-            var collection = {
+            collection = {
                 'type': 'FeatureCollection',
                 'features': myObjects
             }
@@ -177,6 +266,7 @@ function init() {
             var overlay = myObjectManager.objects.getById(feature.id);
 
             myObjectManager.objects.balloon.open(overlay.id);
+
         })   
     })
 }
